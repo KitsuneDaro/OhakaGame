82c69a1b77b3a6a08ffcfd396dfb4e7fe30523ec
kitsunedaro <g2222050@tcu.ac.jp>
2023-10-28 17:33:01 +0900

    update that god move and rotate, and add human scale up

--- scene/object/god/god.gd
+++ scene/object/god/god.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 # move
 var move_able_width = 400
-var move_speed = 150
+var move_speed = 400
+var velocity = Vector2(0, 0)
 
 # rotate
-var max_rotate_radians = PI / 3
-var rotate_reset_speed_radians = PI / 3 # 戻るスピード
-var rotate_radians = 0
-var rotate_velocity_radians = 0
+var rotate_able_radians: float = PI / 2
+var rotate_first_speed_radians: float = PI * 2 / 3 # 戻るスピード
+var rotate_reset_rate: float = 4.0 # 戻るスピード
+var rotate_decay_rate: float = 0.95 # 減衰率
+var rotate_eps_radians: float = 0.005
+var rotate_velocity_radians: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,23 +19,48 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float):
 	move_holizon(delta)
+	rotate_by_move(delta)
 	limit_position()
+	limit_rotate()
 	
 	pass
 
 
-func eval_rotate_radians():
+func rotate_by_move(delta: float):
+	if Input.is_action_just_pressed("ui_left"):
+		rotate_velocity_radians = -rotate_first_speed_radians
+	elif Input.is_action_just_pressed("ui_right"):
+		rotate_velocity_radians = rotate_first_speed_radians
+	elif (Input.is_action_pressed("ui_left")
+		or Input.is_action_pressed("ui_right")):
+		if rotation * rotate_velocity_radians > 0:
+			rotate_velocity_radians = rotate_velocity_radians * rotate_decay_rate
+	elif rotation != 0:
+		rotate_velocity_radians = -rotation * rotate_reset_rate
 	
-	pass
+	rotation += rotate_velocity_radians * delta
 
 
-func move_holizon(delta):
+func limit_rotate():
+	if rotation > rotate_able_radians / 2:
+		rotation = rotate_able_radians / 2
+	elif rotation < -rotate_able_radians / 2:
+		rotation = -rotate_able_radians / 2
+	if abs(rotation) < rotate_eps_radians:
+		rotation = 0
+
+
+func move_holizon(delta: float):
 	if Input.is_action_pressed("ui_left"):
-		position.x -= move_speed * delta
-	if Input.is_action_pressed("ui_right"):
-		position.x += move_speed * delta
+		velocity.x = -move_speed
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = move_speed
+	else:
+		velocity.x = 0
+	
+	position += velocity * delta
 
 
 func limit_position():
--- scene/object/human/human.gd
+++ scene/object/human/human.gd
@@ -3,16 +3,25 @@ extends RigidBody2D
 class_name Human
 signal collision_between_human
 
+var scale_up_first_speed: float = 3.0
+var scale_up_rate: float = 0.1
+
+var time: float = 0
+var origin_scale: Vector2 = scale
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	reset_scale()
 	attach_scale()
 	attach_contact()
 	connect_signal()
 
-func attach_scale():
-	$collision.scale = scale
-	$sprite.scale = scale
-	scale = Vector2(1, 1)
+func reset_scale():
+	scale= Vector2(1, 1)
+
+func attach_scale(scale_rate: float = 1.0):
+	$collision.scale = origin_scale * scale_rate
+	$sprite.scale = origin_scale * scale_rate
 
 func attach_contact():
 	contact_monitor = true
@@ -22,12 +31,20 @@ func connect_signal():
 	body_entered.connect(_on_body_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	
-	pass
+func _process(delta: float):
+	time += delta
+	scale_up()
+	attach_scale(scale_up_rate)
 
-func _on_body_entered(body):
+
+func scale_up():
+	if scale_up_rate < 1:
+		scale_up_rate += time * scale_up_first_speed
+	else:
+		scale_up_rate = 1
+
+
+func _on_body_entered(body: RigidBody2D):
 	if body is Human:
 		if self.get_index() < body.get_index():
 			emit_signal('collision_between_human', self, body)
